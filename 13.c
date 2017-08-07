7484a406918b530e4e6276899849aff019235949
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-07 17:57:07 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+long long num;
+int count = 0;
+printf("Enter an integer: ");
+scanf("%lld", &num);
+while(n != 0)
+num/= 10;
+++count;
+}
+printf("Number of digits: %d", count);
+return 0;
+}
