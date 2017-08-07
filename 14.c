f828773f7fed4086b80b5226680081b8af0028ab
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-07 18:02:43 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+int num;
+printf("Enter a number \n");
+scanf("%d", &num);
+if (num >= 0)
+{
+printf("%d is a positive number \n", number);
+}
+else
+{
+printf("%d is a negative number \n", number);
+}
+return 0;
+}
