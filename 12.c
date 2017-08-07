be527a529e9936017b755ba3317c8a6edc213f60
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-07 17:51:29 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+int num, j, sum = 0;
+printf("Enter a positive integer: ");
+scanf("%d",&num);
+for(j=1; j <= num; ++j)
+{
+sum += j;   
+}
+printf("Sum = %d",sum);
+}
