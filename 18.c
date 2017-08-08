5e49e631742da114cfe46fb63d5026aa5243134c
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-08 11:49:43 +0530

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int lower, higher, j, flag;
+printf("Enter two numbers(intervals): ");
+scanf("%d %d", &lower, &higher);
+printf("Prime numbers between %d and %d are: ", lower, higher);
+while (lower < higher)
+{
+flag = 0;
+for(j = 2; j <= low/2; ++j)
+{
+if(low % j == 0)
+{
+flag = 1;
+break;
+}
+return 0;
+}
