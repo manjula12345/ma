f139dcf25b959526a40cd439e6d2e7f04db0050b
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-08 17:27:23 +0530

    Create 26.c

---
+++ 26.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+  int a, b, j, temp, number, rem;
+  printf("Enter two numbers(intervals): ");
+  scanf("%d %d", &a, &b);
+  printf("Armstrong numbers between %d an %d are: ", a, b);
+  for(j=n1+1; j<n2; ++j)
+  {
+      temp=;
+      number=0;
+      while(temp!=0)
+      {
+          rem=(temp%10);
+          num+=rem*rem*rem;
+          temp/=10;
+      }
+      if(j==number)
+      {
+          printf("%d ",j);
+      }
+  }
+  
+}
