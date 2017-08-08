92b901efef54fa97d0ca654f200e8e53e483f75b
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-08 12:12:16 +0530

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,14 @@
+#include <stdio.h>    
+void main()
+{  
+int count; 
+printf("Even numbers between 1 to 50\n");
+for(count = 1; count <= 50; count++) 
+{
+if(count%2 == 0)
+{ 
+printf("%d ", count);  
+}  
+}  
+   
+} 
