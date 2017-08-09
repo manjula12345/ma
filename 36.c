75886dc41fe34e793005008b926438c39f1b0691
manjula12345 <30342659+manjula12345@users.noreply.github.com>
2017-08-09 17:18:37 +0530

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,21 @@
+// includestdlib.h to execute exit function
+int char char;
+int j;
+
+clrscr();
+void main()
+{
+printf("Print 1 to 5 again and again");
+while(1){
+for(j=1;j<=5;j++)
+
+     printf("\n%d",j);
+
+    ch=getch();
+    if(char=='Q')
+     exit(0);
+
+    }
+
+    getch();
+    }
